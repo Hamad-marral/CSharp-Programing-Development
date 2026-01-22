935c79b31db5457ec0416e1420120d5711a6f5e4
Hamad Khaliq <hammadkhanmarral@gmail.com>
2026-01-22 13:27:10 +0500

    conditional operator

---
+++ conditional-Op.cs
@@ -0,0 +1,19 @@
+using System;
+ class Program
+{
+    static void Main()
+    {
+        int number =10;
+        int AnotherNumber = 20;
+
+        if (number ==10 || AnotherNumber ==2 )
+        {
+           Console.WriteLine("Condition is True");
+           
+        }
+        {
+         else 
+           Console.WriteLine("Condition is False");
+        }
+    }
+}
