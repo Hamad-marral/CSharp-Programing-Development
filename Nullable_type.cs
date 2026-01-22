45364a5a2d5cae15f292fe31dbcf491c1e47c624
Hamad Khaliq <hammadkhanmarral@gmail.com>
2026-01-22 13:56:23 +0500

    Null & Nullable type

---
+++ Nullable_type.cs
@@ -0,0 +1,28 @@
+using System;
+
+ class NullableTypeExample
+{
+    static void Main()
+    {
+        bool? isYouManger = null;
+        {
+            if (isYouManger= True){
+                Console.WriteLine("use is a Manger ");
+                {
+                    elase if (isYouManger = False){
+                        Console.WriteLine("user is not a Manger");
+
+                }
+                else
+                {
+                    Console.WriteLine("user Manger status is unknown");
+                }
+
+            
+            }
+        
+        
+        }}
+    }
+    
+}
