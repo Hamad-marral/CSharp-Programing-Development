4d30ecb3f4768e79bf99f00ad57294fbfa5dca45
Hamad Khaliq <hammadkhanmarral@gmail.com>
2026-01-22 17:23:33 +0500

    array  in c sharp updated

--- array.cs
+++ array.cs
@@ -2,5 +2,15 @@ namespace ENTER_NAMESPACE;
 
 public class array
 {
+  static void Main()
+  {
+    int[] evenNumers= [3]
+    evenNumers[0]=2;
+    evenNumers[1]=4;
+    evenNumers[2]=6;
+
+
+    Console.WriteLine(evenNumers[0]);
+  }
 
 }
